26fe02a8392d5f258b3f3100f716c2e7af2b6f54
NetoRocha97 <jose.rocha@dcx.ufpb.br>
2020-04-17 21:52:06 -0300

    Solução ruim

--- src/curso/Programa.java
+++ src/curso/Programa.java
@@ -12,38 +12,34 @@ public class Programa {
 	public static void main(String[] args) throws ParseException {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		System.out.print("Quarto numero: ");
-		int numero = Integer.parseInt(sc.nextLine());
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(sc.nextLine());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(sc.nextLine());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Erro na reserva: a data de check-out deve ser posterior à data de check-in");
+
+		System.out.print("Número do quarto: ");
+		int numero = sc.nextInt();
+		System.out.print("Data do check-in (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Data do check-out (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Erro na reserva: a data do check-out deve ser posterior à data do check-in");
 		}
 		else {
 			Reserva reserva = new Reserva(numero, checkIn, checkOut);
-			System.out.println("Reserva: "+ reserva);
-			
+			System.out.println("Reserva: " + reserva);
+
 			System.out.println();
-			System.out.print("Insira dados para atualizar a reserva: ");
-			System.out.print("Data de check-in ()dd/MM/yyyy: ");
-			checkIn = sdf.parse(sc.nextLine());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sc.nextLine());
+			System.out.println("Digite os dados para atualizar a reserva: ");
+			System.out.print("Data do check-in (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Data do check-out (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
 			
-			Date agora = new Date();
-			if(checkIn.before(agora) || checkOut.before(agora)) {
-				System.out.println("Erro na reserva: As datas de reserva para atualização devem ser futuras.");
-			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Erro na reserva: a data de check-out deve ser posterior à data de check-in");
+			String erro =  reserva.atualizacaoData(checkIn, checkOut);
+			if(erro != null){
+				System.out.println("Erro na reserva: " + erro);
 			}
 			else {
-			reserva.atualizacaoData(checkIn, checkOut);
-			System.out.println("Reserva: "+ reserva);
+				System.out.println("Reserva: "+ reserva);
 			}
 		}
 		
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -8,11 +8,10 @@ public class Reserva {
 	private Integer numeroDoQuarto;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reserva(Integer numeroDoQuarto, Date checkIn, Date checkOut) {
-
 		this.numeroDoQuarto = numeroDoQuarto;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -33,15 +32,23 @@ public class Reserva {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duracao() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void atualizacaoData (Date checkIn, Date checkOut) {
+
+	public String atualizacaoData(Date checkIn, Date checkOut) {
+		Date agora = new Date();
+		if (checkIn.before(agora) || checkOut.before(agora)) {
+			return "Erro na reserva: as datas da reserva para atualização devem ser futuras";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Erro na reserva: a data do check-out deve ser posterior à data do check-in";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
